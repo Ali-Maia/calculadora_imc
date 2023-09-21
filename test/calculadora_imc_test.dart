91028dc958c95a898f508f0c764a7b936e2f962b
Ali-Maia <aliciaengcomp@gmail.com>
2023-09-20 21:38:43 -0300

    adicionando testes

--- test/calculadora_imc_test.dart
+++ test/calculadora_imc_test.dart
@@ -2,7 +2,33 @@ import 'package:calculadora_imc/functions/functions.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calcularImc(1.5, 60), equals(26.7));
+group("Calcula o valor do IMC: ", () {
+    var valuesToTest = {
+      {'altura': 1.6, 'peso': 80}: 31.2,
+      {'altura': 1.9, 'peso': 90}: 24.9,
+    };
+    valuesToTest.forEach((values, expected) {
+      test('$values: $expected', () {
+        expect(
+            calcularImc(double.parse(values['altura'].toString()),
+                double.parse(values['peso'].toString())),
+            equals(expected));
+      });
+    });
+  });
+  group("Calcula o valor do IMC informando valores zerados: ", () {
+    var valuesToTest = {
+      {'altura': 0, 'peso': 80}: 0,
+      {'altura': 1.6, 'peso': 0}: 0,
+    };
+    valuesToTest.forEach((values, expected) {
+      test('$values: $expected', () {
+        expect(
+            calcularImc(double.parse(values['altura'].toString()),
+                double.parse(values['peso'].toString())),
+            equals(expected));
+      });
+    });
   });
 }
+
